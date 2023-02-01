bda8153f3b489dc97d821a5f957c1993864d1724
Madhav007 <madhavaprashath669@gmail.com>
2023-02-01 19:11:21 +0530

    Added pin popup for all pins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import {render} from 'react-dom';
 import { useEffect} from 'react';
-import Map, {Marker, NavigationControl, Popup} from 'react-map-gl';
-import Brightness3Icon from '@mui/icons-material/Brightness3';
+import Map, {Marker, Popup} from 'react-map-gl';
 import StarIcon from '@mui/icons-material/Star';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
 import axios from "axios"
+import {format} from "timeago.js"
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_API; // Set your mapbox token here
 
 function App() {
@@ -17,16 +16,17 @@ function App() {
     height:200,
     zoom: 14
   });
-  const [showPopup, setShowPopup] = useState(true);
   const [pins, setPins] = useState([]);
-
+  
+  const [currentPlaceId, setCurrentPlaceId] = useState("");  // currentPlaceId - is the _id of the pin that is currenly open.
 
   useEffect(()=>{
     const getPins = async ()=>{
       try{  
         const res = await axios.get("/pins");
-        console.log("JEREE",res);
+        // console.log("JEREE",res);
         setPins(res.data);
+        // console.log("Pins are: ",pins);
       }catch(err){
         console.log(err);
       }
@@ -34,7 +34,14 @@ function App() {
     getPins();
   },[])
 
+useEffect(()=>{
+  console.log(currentPlaceId);
+},[currentPlaceId])
 
+  const handlePinOpen = (id)=>{
+    console.log(typeof id);
+    setCurrentPlaceId(id);
+  }
   return (
     <div className="App">
       <Map
@@ -43,36 +50,41 @@ function App() {
         style={{width: "100vw", height: "100vh"}}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={MAPBOX_TOKEN}
+        
         >
-        {pins.map( pin =>(
+        {pins.map( (pin) =>(
+  
           <>
-          <Marker longitude={pin.longitude} latitude={pin.latitude} color="red" />
-          <NavigationControl />
-          {showPopup && (
-            <Popup longitude={pin.longitude} latitude={pin.latitude}
-            anchor="top"
-            onClose={() => setShowPopup(false)}>
-            <div className="card">
-            <label >Place</label>
-            <h4>Effiel TOwer</h4>
-            <label >Review</label>
-            <p className="desc">Very beautiful place</p>
-            <label >Rating</label>
-            <div className="stars">
-            <StarIcon className='star'></StarIcon>
-            <StarIcon className='star'></StarIcon>
-            <StarIcon className='star'></StarIcon>
-            <StarIcon className='star'></StarIcon>
-            <StarIcon className='star'></StarIcon>
-            </div>
-            
-            <label >Information</label>
-            <span className="username">Created by: <b>Madhav</b></span>
-            <span className="date">1 hr ago</span>
-            </div>
-          </Popup>)}
+          
+          {console.log(`pin._id: ${pin._id}, currentPlaceId: ${currentPlaceId}`)}
+          {console.log(`Comparison result: ${pin._id === currentPlaceId}`)}
+            <Marker   longitude={pin.longitude} latitude={pin.latitude} color="red" onClick={()=>handlePinOpen(pin._id)}  />
+            { pin._id === currentPlaceId && 
+                <Popup  longitude={pin.longitude} latitude={pin.latitude} anchor="top" closeButton={true}  closeOnClick={false} onClose={()=>setCurrentPlaceId("")} >
+                    <div className="card">
+                      <label >Place</label>
+                      <h4>Effiel TOwer</h4>
+                      <label >Review</label>
+                      <p className="desc">{pin.desc}</p>
+                      <label >Rating</label>
+                      <div className="stars">
+                        <StarIcon className='star'></StarIcon>
+                        <StarIcon className='star'></StarIcon>
+                        <StarIcon className='star'></StarIcon>
+                        <StarIcon className='star'></StarIcon>
+                        <StarIcon className='star'></StarIcon>
+                      </div>
+                      
+                      <label >Information</label>
+                      <span className="username">Created by: <b>{pin.username}</b></span>
+                      <span className="date">{format(pin.createdAt)}</span>
+                    </div>
+              </Popup>
+            }
+
           </>
-          ))}
+          ))
+        }
       </Map>
     </div>
   );
